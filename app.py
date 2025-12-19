e94c75c9e2beb506fe9b6dafa90f094730b0a03f
Sm in tech <ellaelearnor66@gmail.com>
2025-12-18 19:56:21 -0500

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-from psycopg2.extras import RealDictCursor
-from config import DATABASE_URL
-
-def get_db():
-    conn = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
-    return conn
